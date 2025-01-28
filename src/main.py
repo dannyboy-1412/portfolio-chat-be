0329ed324df09a43e5f36241c6342abe5e02a5d4
dannyboy-1412 <danielantorodri@gmail.com>
2025-01-28 12:53:49 +0530

    added cors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.endpoints.chat import chat_router
 from src.api.endpoints.resume import resume_router
 
@@ -7,6 +8,14 @@ api_v1_router = APIRouter(prefix="/api/v1")
 
 api_v1_router.include_router(chat_router, prefix="/chat")
 api_v1_router.include_router(resume_router, prefix="/resume")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/") 
 async def main_route():     
